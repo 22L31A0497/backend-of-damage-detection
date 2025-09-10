8a54cce10564931283f5a64e17c0b66d4eea50e6
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-09-10 21:09:04 +0530

    gdown added

--- detection/load_model.py
+++ detection/load_model.py
@@ -1,36 +1,18 @@
 import os
-import requests
+import gdown
 from ultralytics import YOLO
 
-MODEL_PATH = "/tmp/best.pt"  # stored in temp folder at runtime
-
-def download_file_from_google_drive(file_id, dest_path):
-    URL = "https://drive.google.com/uc?export=download"
-
-    session = requests.Session()
-    response = session.get(URL, params={"id": file_id}, stream=True)
-    
-    # If Google Drive requires confirmation (large files)
-    for key, value in response.cookies.items():
-        if key.startswith("download_warning"):
-            response = session.get(URL, params={"id": file_id, "confirm": value}, stream=True)
-            break
-
-    with open(dest_path, "wb") as f:
-        for chunk in response.iter_content(32768):
-            if chunk:
-                f.write(chunk)
-
+MODEL_PATH = "/tmp/best.pt"
+FILE_ID = "1jeL98r_B4H7JJpnU5geZKTjp5AhdbAOm"
 
 def download_model():
-    file_id = "1jeL98r_B4H7JJpnU5geZKTjp5AhdbAOm"  # your file ID
     if not os.path.exists(MODEL_PATH):
-        print("⬇️ Downloading YOLO model from Google Drive...")
-        download_file_from_google_drive(file_id, MODEL_PATH)
+        print("⬇️ Downloading YOLO model from Google Drive with gdown...")
+        url = f"https://drive.google.com/uc?id={FILE_ID}"
+        gdown.download(url, MODEL_PATH, quiet=False)
     else:
         print("✅ Model already downloaded.")
     return MODEL_PATH
-
 
 def load_model():
     path = download_model()
